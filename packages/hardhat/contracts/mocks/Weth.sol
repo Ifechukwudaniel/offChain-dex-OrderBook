751f4f2155cd6bc786091ae7167a76a80c7806c8
Angry Coding <mailidanielifechukwu@gmail.com>
2023-03-01 19:07:55 +0100

    mock token

---
+++ packages/hardhat/contracts/mocks/Weth.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Weth is ERC20 {
+  constructor() ERC20("Weth", "Wrapped Ether") {}
+
+  function faucet(address to, uint amount) public {
+    _mint(to, amount);
+  }
+}
