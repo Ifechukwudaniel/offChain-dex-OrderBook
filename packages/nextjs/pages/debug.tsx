0373b0da36547fbf9c45809bfc9eee833a8824d1
Angry Coding <mailidanielifechukwu@gmail.com>
2023-03-02 02:43:26 +0100

    debug  contracts

--- packages/nextjs/pages/debug.tsx
+++ packages/nextjs/pages/debug.tsx
@@ -6,18 +6,14 @@ const Debug: NextPage = () => {
     <>
       <div className="flex justify-center">
         <div className="grid grid-cols-1 lg:grid-cols-6 py-12 px-10 lg:gap-12 w-full max-w-7xl my-0">
-          <ContractUI contractName="YourContract" />
+          <ContractUI contractName="Bat" />
+          <ContractUI contractName="Dai" />
+          <ContractUI contractName="Rep" />
+          <ContractUI contractName="Usdt" />
+          <ContractUI contractName="Wbtc" />
+          <ContractUI contractName="Weth" />
+          <ContractUI contractName="Zrx" />
         </div>
-      </div>
-      <div className="text-center mt-8 bg-secondary p-10">
-        <h1 className="text-4xl my-0">Debug Contracts</h1>
-        <p className="text-neutral">
-          You can debug & interact with your deployed contracts here.
-          <br /> Check{" "}
-          <code className="italic bg-base-300 text-base font-bold [word-spacing:-0.5rem] px-1">
-            packages / nextjs / pages / debug.tsx
-          </code>{" "}
-        </p>
       </div>
     </>
   );
