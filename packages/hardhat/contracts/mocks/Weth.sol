d85154a77d5ac3405b4f038bcabcf8f9889eea4c
Angry Coding <mailidanielifechukwu@gmail.com>
2023-03-02 17:56:55 +0100

    changed symbol

--- packages/hardhat/contracts/mocks/Weth.sol
+++ packages/hardhat/contracts/mocks/Weth.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Weth is ERC20 {
-  constructor() ERC20("Wrapped Ether", "Weth") {}
+  constructor() ERC20("Wrapped Ether", "WETH") {}
 
   function faucet(address to, uint amount) public {
     _mint(to, amount);
