f9da9650349418692dd1df50d79279006c815e14
Angry Coding <mailidanielifechukwu@gmail.com>
2023-03-02 23:37:00 +0100

    faucet page and other changes

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -3,6 +3,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  theme: {
+    minHeight: {
+      "1/2": "50%",
+    },
+  },
 };
 
 module.exports = nextConfig;
